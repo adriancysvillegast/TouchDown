0661a44fae086866438fd28e6771424b679afe9a
Adriancys Villegas <adriancysvillegast@gmail.com>
2023-08-09 18:37:33 -0400

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Adriancys Jesus Villegas Toro on 9/8/23.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
